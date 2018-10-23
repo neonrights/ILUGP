a1eaa24224021d8ca0c9a0412cadad2e308a1601
neonrights <nicholasfarn@gmail.com>
2018-10-23 01:15:01 +0000

    converted transformer implementation to only attention mechanism

--- custom.py
+++ custom.py
@@ -3,30 +3,26 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 # accepts input in [ batch x channels x shape ] format
-class Transformer(nn.Module):
-    def __init__(self, in_channels, heads, dropout=None, masked=False):
+class Attention(nn.Module):
+    def __init__(self, in_channels, heads, dropout=None):
         assert in_channels % heads == 0
 
         super().__init__()
         self.in_channels = in_channels
         self.heads = heads
-        self.masked = masked
-        self.dropout = 0.5
-        self.produce_qkv = nn.Conv1d(in_channels, 3*in_channels, 1)
+        self.dropout = dropout
 
-    def forward(self, input_):
-        qkv = self.produce_qkv(input_)
-        queries, keys, values = torch.split(qkv, self.in_channels, dim=1) 
+    def forward(self, queries, keys, values, mask=None):
         attention = torch.bmm(keys.permute(0,2,1), queries) / self.in_channels**0.5
-        if self.masked:
-            pass # mask future input
+        if mask is not None:
+            attention = attention.masked_fill(mask == 0, -1e9)
         
         attention = F.softmax(attention, dim=1)
         if dropout is not None:
             attention = F.dropout(attention, self.dropout)
 
         output = torch.bmm(values, attention)
-        return input_ + output
+        return output
 
 # adds positional encodings
 class PositionalEncoding(nn.Module):
@@ -37,3 +33,4 @@ class PositionalEncoding(nn.Module):
         positional_encodings = torch.sin(torch.ger(denominator, numerator))
         return input_ + positional_encodings
  
+
